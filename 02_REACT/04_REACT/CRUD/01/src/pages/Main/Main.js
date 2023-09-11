8fbdaba4fe5e03185b841f846540c8a6332ec51c
sangminka <141594835+sangminka@users.noreply.github.com>
2023-09-11 17:48:28 +0900

    main 만들기

--- 02_REACT/04_REACT/CRUD/01/src/pages/Main/Main.js
+++ 02_REACT/04_REACT/CRUD/01/src/pages/Main/Main.js
@@ -2,7 +2,17 @@ import React from 'react'
 import './Main.css'
 function Main() {
   return (
-    <div>Main</div>
+    <div className='main'>
+      <div className='mainTitle'>
+        <h1>전체 게시판</h1>
+      </div>
+      <div className='mainForm'>
+         <p>테스트 합니다.</p>
+         
+      </div>
+
+      
+    </div>
   )
 }
 
